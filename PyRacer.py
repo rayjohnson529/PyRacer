45d4e0abe0e2a059953c6ae83c78c236b5034b3a
Ray Johnson <rayjohnson529@outlook.com>
2020-04-26 19:38:47 -0500

    Car Related Things

--- PyRacer.py
+++ PyRacer.py
@@ -1,6 +1,6 @@
 '''
 @author Ray Johnson
-PyRacer is a Python game where you avoid objects.
+PyRacer is a Python game where you avoid other cars.
 Racing isn't part of the game. Sorry about that.
 '''
 
@@ -38,15 +38,19 @@ pause = False
 # impose time on game
 clock = pygame.time.Clock()
 carImg = pygame.image.load('racecarSm.png')
+otherCarImg = pygame.image.load('otherCarSm.png')
 carCrashImg = pygame.image.load('racecarSmCrashed2.png')
 carIcon = pygame.image.load('racecarIcon.png')
 pygame.display.set_icon(carIcon)
 pygame.mixer.music.load('engine.wav')
+bg = pygame.image.load('road.png').convert()
 
 crash_sound = pygame.mixer.Sound(file='crash.wav')
+otherCarCount = 0
 
 # define width of car
 (car_width, car_height) = carImg.get_rect().size
+(otherCar_width, otherCar_height) = otherCarImg.get_rect().size
 
 def button(msg, x,y,w,h,ic,ac,action=None):
     mouse = pygame.mouse.get_pos()
@@ -85,12 +89,14 @@ def display_highest_score(score):
     textviz = font.render(text, True, dark_iceberg)
     gameDisplay.blit(textviz,(display_width-len(text)*9,0))
     
-def things(thingx, thingy, thingw,thingh, color):
-    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
-    
+#def things(thingx, thingy, thingw,thingh, color):
+#    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
     
 def car(x, y):
     gameDisplay.blit(carImg, (x,y))
+    
+def otherCar(x,y):
+    gameDisplay.blit(otherCarImg, (x,y))
 
 def quit_game():
     pygame.quit()
@@ -185,24 +191,33 @@ def game_intro():
         
 def game_loop(): 
     global pause
+    global otherCar_width
+    global otherCar_height
     pygame.mixer.music.play(-1)
     x = (display_width * 0.45)
     y = (display_height * 0.8)
     x_change = 0
+   # otherCars = 1
+    otherCarStartX = random.randrange(0, display_width)
+    otherCarStartY = -600
+    otherCarSpeed = 7.8
     
-    thing_startx = random.randrange(0, display_width)
-    thing_starty = -600
-    thing_speed = 7.8
-    thing_width = 90
-    thing_height = 90
-    thing_count = 1
+    
+#    thing_startx = random.randrange(0, display_width)
+#    thing_starty = -600
+#    thing_speed = 7.8
+#    thing_width = 90
+#    thing_height = 90
+#    thing_count = 1
     
     dodged = 0
     gameExit = False
     high_score = get_high_score()
-    impossible_mode = False
+    #impossible_mode = False
 
     while not gameExit:
+        gameDisplay.blit(bg, [0,0])
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -212,6 +227,7 @@ def game_loop():
                 # left arrow
                 if event.key == pygame.K_LEFT:
                     x_change = -12
+                    
                     
                 # right arrow
                 if event.key == pygame.K_RIGHT:
@@ -228,12 +244,14 @@ def game_loop():
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     x_change = 0
+                    
         x += x_change
-        gameDisplay.fill(white)
+        #gameDisplay.fill(white)
         
         # things(thingx, thingy, thingw,thingh, color)
-        things(thing_startx, thing_starty, thing_width, thing_height, black)
-        thing_starty += thing_speed
+#        things(thing_startx, thing_starty, thing_width, thing_height, black)
+        otherCar(otherCarStartX,otherCarStartY)
+        otherCarStartY += otherCarSpeed
         
         
         car(x,y)
@@ -249,20 +267,26 @@ def game_loop():
                     pickle.dump(high_score,file)
             crash()
             
-        if thing_starty > display_height:
-            thing_starty = 0 - thing_height
-            thing_startx = random.randrange(0, display_width)
+        if otherCarStartY > display_height:
+            otherCarStartY = 0 - otherCar_height
+            otherCarStartX = random.randrange(0, display_width)
             dodged+=1
-            thing_speed+=.8
-            if impossible_mode == True:
-                thing_width = thing_width + (thing_width*1.4)
-            thing_count +=2 
-                
+            otherCarSpeed+=.8
+#            if impossible_mode == True:
+#                otherCar_width = otherCar_width + (otherCar_width*1.4)
+            #thing_count +=2 
+#            if y < thing_starty + thing_height:
+#            print('step 1')
+#            if x > thing_startx and x < thing_startx + thing_width or x+car_width > thing_startx and x + car_width < thing_startx + thing_width:
+#                print('x crossover')
+#                if dodged >= high_score:
+#                    high_score = dodged
+#                    with open('score.dat','wb') as file:
+#                        pickle.dump(high_score,file)
+#                car_crashed(x,y)
             
-        if y < thing_starty + thing_height:
-            print('step 1')
-            if x > thing_startx and x < thing_startx + thing_width or x+car_width > thing_startx and x + car_width < thing_startx + thing_width:
-                print('x crossover')
+        if y < otherCarStartY + otherCar_height:
+            if x > otherCarStartX and x < otherCarStartX + otherCar_width or x+car_width > otherCarStartX and x + car_width < otherCarStartX + otherCar_width:
                 if dodged >= high_score:
                     high_score = dodged
                     with open('score.dat','wb') as file:
